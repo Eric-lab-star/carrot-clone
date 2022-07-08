a256a5f38ec4bd9489482fc2f1210e4076cc78a5
Eric Kim <cyon256@icloud.com>
2022-07-08 23:34:42 +0900

    tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,tsx,ts}", "./components/**/*.{js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
