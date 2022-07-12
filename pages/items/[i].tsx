e11b3c6666108c279d9c8c574eb51cdf3f1f73fb
Eric Kim <cyon256@icloud.com>
2022-07-12 12:12:58 +0900

    items detail page css, markup complete

--- pages/items/[i].tsx
+++ pages/items/[i].tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const ItemDetail: NextPage = () => {
   return (
     <div className="px-5 py-10">
-      <div>
+      <div className="mb-4">
         <div className="h-80 bg-slate-300" />
         <div className="flex items-center space-x-3 py-2 border px-2">
           <div className="h-12 w-12 rounded-full bg-gray-200" />
@@ -14,10 +14,10 @@ const ItemDetail: NextPage = () => {
             </p>
           </div>
         </div>
-        <div className="mt-9">
+        <div className="mt-5">
           <h1 className="font-extrabold text-2xl">Galaxy S50</h1>
           <p className="text-xl font-bold">$140</p>
-          <p className="font-thin py-4">
+          <p className="text-base font-thin py-4 selection:bg-amber-300 selection:text-white">
             My money&apos;s in that office, right? If she start giving me some
             bullshit about it ain&apos;t there, and we got to go someplace else
             and get it, I&apos;m gonna shoot you in the head then and there.
@@ -27,9 +27,11 @@ const ItemDetail: NextPage = () => {
             and that ni**a Winston or anybody else is in there, you the first
             motherfucker to get shot. You understand?
           </p>
-          <div className="flex justify-between w-full">
-            <button>Talk to seller</button>
-            <button>
+          <div className="flex justify-between w-full space-x-2 ">
+            <button className="flex-1 bg-amber-400 py-2 rounded-md hover:text-white hover:bg-amber-500 focus:ring-1 focus:outline-none focus:ring-offset-1 focus:ring-amber-400">
+              Talk to seller
+            </button>
+            <button className="w-10 h-10 flex justify-center items-center text-gray-500 hover:text-amber-400 hover:bg-amber-200 hover:rounded-full rounded-full focus:outline-none focus:ring-2 focus:ring-amber-500">
               <svg
                 className="h-6 w-6 "
                 xmlns="http://www.w3.org/2000/svg"
@@ -49,14 +51,18 @@ const ItemDetail: NextPage = () => {
           </div>
         </div>
       </div>
-      <div>
-        <h2>Similar items</h2>
-        <div>
+      <div className="border-t border-gray-300 pt-3">
+        <h2 className="font-semibold text-2xl mb-2">Similar items</h2>
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 sm:grid-cols-3">
           {[1, 2, 3, 4, 5, 6].map((_, i) => (
             <div key={i}>
-              <div />
-              <h3>Galaxy S60</h3>
-              <p>$6</p>
+              <div className="w-full h-40 bg-gray-400 rounded-md mb-2" />
+              <div className="flex items-center justify-start space-x-2">
+                <h3 className="font-medium text-gray-800">Galaxy S60</h3>
+                <p className="text-sm rounded-md select-none bg-amber-400 p-1 text-white">
+                  $6
+                </p>
+              </div>
             </div>
           ))}
         </div>
