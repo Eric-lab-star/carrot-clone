ea724842d92bfb9997a556d27dc1a700c84448df
eric-lab-star <cyon256@icloud.com>
2022-07-16 23:35:19 +0900

    add link to video

--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -1,20 +1,25 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Stream: NextPage = () => {
   return (
     <div className=" py-16 grid md:grid-cols-2 lg:grid-cols-3 ">
       {[1, 2, 1, 1, 1].map((v, i) => {
         return (
-          <div key={i} className="space-y-3 pt-3 pb-2 px-4">
-            <div className="w-full aspect-video bg-slate-300 rounded-md shadow-md " />
-            <div className="flex justify-start space-x-2 items-start">
-              <div className="w-8 h-8 rounded-full bg-gray-500" />
-              <div>
-                <h1 className="text-base leading-4">Lets try something new</h1>
-                <span className="text-xs font-thin">1시간 전</span>
+          <Link href={`stream/${i}`} key={i}>
+            <div className="space-y-3 pt-3 pb-2 px-4">
+              <div className="w-full aspect-video bg-slate-300 rounded-md shadow-md " />
+              <div className="flex justify-start space-x-2 items-start">
+                <div className="w-8 h-8 rounded-full bg-gray-500" />
+                <div>
+                  <h1 className="text-base leading-4">
+                    Lets try something new
+                  </h1>
+                  <span className="text-xs font-thin">1시간 전</span>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         );
       })}
       <button className="fixed bottom-3 right-5 border-transparent bg-amber-400 rounded-full text-white p-2 shadow-lg hover:bg-amber-500 transition-colors">
