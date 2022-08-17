830d244697e7696daaeccf83477dd268a06b7942
eric-lab-star <cyon256@icloud.com>
2022-08-17 11:11:15 +0900

    post db

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,16 +1,45 @@
 import Btn from "@components/btn";
 import Layout from "@components/layout";
 import TextArea from "@components/textArea";
+import { Post } from "@prisma/client";
+import useMutation from "libs/client/useMutation";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface IQuestion {
+  question: string;
+}
+
+interface IWriteRes {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const { register, handleSubmit } = useForm<IQuestion>();
+  const [post, { loading, data }] = useMutation<IWriteRes>("/api/posts");
+  const onValid = (data: IQuestion) => {
+    if (loading) return;
+    post(data);
+  };
+  const router = useRouter();
+  useEffect(() => {
+    if (data && data.ok) router.push(`/community/${data.post.id}`);
+  }, [data, router]);
   return (
     <Layout title="Write">
       <div className=" px-4">
-        <div className="mt-2">
-          <TextArea placeholder="Please write something here" label=" Title" />
-          <Btn>post</Btn>
-        </div>
+        <form onSubmit={handleSubmit(onValid)} className="mt-2">
+          <TextArea
+            register={register("question", { required: true, minLength: 5 })}
+            required
+            placeholder="Please write something here"
+            label=" Title"
+          />
+          <Btn>{loading ? "Loading" : "Submit"}</Btn>
+        </form>
       </div>
     </Layout>
   );
